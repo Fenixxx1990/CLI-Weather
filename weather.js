ba44df3eeb3c2643fa1fdc2e9755311a25f51c1e
Fenix <fenixxx1990@yandex.ru>
2026-03-04 22:15:02 +0400

    first request

--- weather.js
+++ weather.js
@@ -1,10 +1,25 @@
 #!/usr/bin/env node
 import { getArgs } from "./helpers/args.js";
-import { printHelp } from "./services/log.service.js";
+import { getWeather } from "./services/api.service.js";
+import { printHelp, printSuccess, printError } from "./services/log.service.js";
+import { saveKeyValue, TOKEN_DICTIONARY } from "./services/storage.service.js";
+
+const saveToken = async (token) => {
+  if (!token.length) {
+    printError("Токен не указан");
+    return;
+  }
+  try {
+    await saveKeyValue(TOKEN_DICTIONARY.token, token);
+    printSuccess("Токен сохранён");
+  } catch (e) {
+    printError(e.message);
+  }
+};
 
 const initCLI = () => {
   const args = getArgs(process.argv);
-  console.log(args);
+  // console.log(args);
   if (args.h) {
     printHelp();
   }
@@ -12,8 +27,9 @@ const initCLI = () => {
     //сохранить город
   }
   if (args.t) {
-    //сохранить токен
+    return saveToken(args.t);
   }
+  getWeather("engels");
   //вывести погоду
 };
 
